3bf7dbc61208bd84c13dbc936a2624e409ce3715
ivandjoh <ivandi.djoh@gmail.com>
2023-02-02 15:30:13 +0800

    first init

---
+++ README.md
@@ -0,0 +1,5 @@
+## My Spring JWT
+
+### About
+This is how i `Secure` my Spring Boot Application with JWT
+
---
+++ src/test/java/com/ivandjoh/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
