58a88d3c5e690f3b6ea42915b141ea151f56b9ac
ivandjoh <ivandi.djoh@gmail.com>
2023-02-02 19:57:52 +0800

    Create AuthController

---
+++ src/main/java/com/ivandjoh/springjwt/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.ivandjoh.springjwt.controller;
+
+import com.ivandjoh.springjwt.service.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AuthController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+
+        LOGGER.debug("Generating token for {}", authentication.getName());
+
+        String token = this.tokenService.generateToken(authentication);
+
+        LOGGER.debug("Toke Guarantee: {}", token);
+
+        return token;
+    }
+}
