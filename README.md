6db19684dffc33be7d17b8f37114374224c19f65
ivandjoh <ivandi.djoh@gmail.com>
2023-02-02 16:23:48 +0800

    Generate Public, Private and Keypair

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 ### About
 This is how i `Secure` my Spring Boot Application with JWT
 
+```bash
+THIS PROJECT STILL IN DEVELOPMENT
+```
+
+***
+
+- Create pem file with openssl
+```bash
+openssl genrsa -out keypair.pem 2048
+```
+
+- Create public key from pem file
+```bash
+openssl rsa -in keypair.pem -pubout -outform PEM -out public.pem
+```
+
+- Create private key from pem file
+```bash
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem
+```
--- src/main/java/com/ivandjoh/springjwt/config/SecurityConfig.java
+++ src/main/java/com/ivandjoh/springjwt/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -30,6 +31,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeRequests(auth -> auth
                         .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
