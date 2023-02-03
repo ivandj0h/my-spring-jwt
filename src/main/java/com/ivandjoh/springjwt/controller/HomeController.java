261be54d95e07493f2c84a4a25f0f1e540cc4b4b
ivandjoh <ivandi.djoh@gmail.com>
2023-02-03 19:57:03 +0800

    Readme Updated!

--- src/main/java/com/ivandjoh/springjwt/controller/HomeController.java
+++ src/main/java/com/ivandjoh/springjwt/controller/HomeController.java
@@ -1,15 +1,23 @@
 package com.ivandjoh.springjwt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.logging.Logger;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HomeController {
 
-    @GetMapping
+    private static final Logger LOGGER = Logger.getLogger(HomeController.class.getName());
+
+    @GetMapping("")
     public String home(Principal principal) {
+
+        LOGGER.info("Hello " + principal.getName());
+
         return "Hello " + principal.getName();
     }
 }
